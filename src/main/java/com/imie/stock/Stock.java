630635066bf0cd525ec5af107e6fce143f1bebe1
ThierryChampot <thierry.champot@gmail.com>
2018-06-20 12:04:08 +0200

    WIP correcting dao and ctrl

--- src/main/java/com/imie/stock/Stock.java
+++ src/main/java/com/imie/stock/Stock.java
@@ -1,10 +1,17 @@
 package com.imie.stock;
 
+import com.imie.stock.daos.ProductDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Stock {
+	@Autowired
+	ProductDao productDao;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Stock.class, args);
 	}
