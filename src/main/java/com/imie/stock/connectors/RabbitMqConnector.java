db49bdbb92344fdea09fa16b4095eb2b75012fc3
ThierryChampot <thierry.champot@gmail.com>
2018-06-21 14:43:09 +0200

    added connection to rabbitMq local server

---
+++ src/main/java/com/imie/stock/connectors/RabbitMqConnector.java
@@ -0,0 +1,10 @@
+package com.imie.stock.connectors;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.context.annotation.Configuration;
+
+@EnableRabbit
+@Configuration
+public class RabbitMqConnector {
+}
+
---
+++ src/main/java/com/imie/stock/connectors/Receiver.java
@@ -0,0 +1,20 @@
+package com.imie.stock.connectors;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener
+public class Receiver {
+
+    @RabbitListener(queues = "produits-in")
+    public void receiveMessageIn(final Message message) {
+        System.out.println("Received message as generic: " +  new String (message.getBody()));
+    }
+
+    @RabbitListener(queues = "produits-out")
+    public void receiveMessageOut(final Message message) {
+        System.out.println("Received message as generic: " +  new String (message.getBody()));
+    }
+}
